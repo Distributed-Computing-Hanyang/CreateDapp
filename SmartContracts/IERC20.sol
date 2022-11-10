52c07ba4742eee8f762d98fe50277f884cfddcda
DCC-JEHA <114203052+DCC-JEHA@users.noreply.github.com>
2022-11-10 17:47:27 +0900

    Update IERC20.sol

--- SmartContracts/IERC20.sol
+++ SmartContracts/IERC20.sol
@@ -1,6 +1,6 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.8.0;
 
-contract IERC20 {
+interface IERC20 {
     function totalSupply() external view returns (uint256);
     function balanceOf(address user) external view returns (uint256);
     function transfer(address to, uint256 value) external returns (bool);
