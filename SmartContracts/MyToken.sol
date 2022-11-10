7a91792a4648532ff8fd18a58ffb4c03b7539e90
DCC-JEHA <114203052+DCC-JEHA@users.noreply.github.com>
2022-11-10 17:45:16 +0900

    Update MyToken.sol

--- SmartContracts/MyToken.sol
+++ SmartContracts/MyToken.sol
@@ -1,12 +1,11 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.8.0;
 
 import "./BasicToken.sol";
 
 contract MyToken is BasicToken {
-    uint8 public constant decimals = 18;
-    uint256 public constant INITIAL_SUPPLY = 10000 * (10 ** uint256(decimals));
+    uint256 public constant INITIAL_SUPPLY = 1000;
     
-    constructor() public {
+    constructor() {
         totalSupply_ = INITIAL_SUPPLY;
         _balances[msg.sender] = INITIAL_SUPPLY;
         emit Transfer(address(0), msg.sender, INITIAL_SUPPLY);
